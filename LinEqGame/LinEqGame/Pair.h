4a2ccc6bcb5e5f24df6e317acfeb842023cda749
Jason Yu <yujason2@gmail.com>
2013-11-19 13:10:04 -0800

    Workspace files

---
+++ LinEqGame/LinEqGame/Pair.h
@@ -0,0 +1,14 @@
+//
+//  Pair.h
+//  LinEqGame
+//
+//  Created by Jason Yu on 11/19/13.
+//  Copyright (c) 2013 Team15. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Pair : NSObject
+@property float x;
+@property float y;
+@end
---
+++ LinEqGame/LinEqGame/Pair.m
@@ -0,0 +1,13 @@
+//
+//  Pair.m
+//  LinEqGame
+//
+//  Created by Jason Yu on 11/19/13.
+//  Copyright (c) 2013 Team15. All rights reserved.
+//
+
+#import "Pair.h"
+
+@implementation Pair
+
+@end
