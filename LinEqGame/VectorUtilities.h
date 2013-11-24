57e8ae765c2a743ec1832572a8a3bdeab795b256
Jason Yu <yujason2@gmail.com>
2013-11-24 13:52:48 -0800

    utility functions to manipulate vectors

---
+++ LinEqGame/VectorUtilities.h
@@ -0,0 +1,13 @@
+//
+//  VectorUtilities.h
+//  LinEqGame
+//
+//  Created by Jason Yu on 11/24/13.
+//  Copyright (c) 2013 Team15. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VectorUtilities : NSObject
+
+@end
---
+++ LinEqGame/VectorUtilities.m
@@ -0,0 +1,13 @@
+//
+//  VectorUtilities.m
+//  LinEqGame
+//
+//  Created by Jason Yu on 11/24/13.
+//  Copyright (c) 2013 Team15. All rights reserved.
+//
+
+#import "VectorUtilities.h"
+
+@implementation VectorUtilities
+
+@end
